b780241030a81d18e90bcc142d8317beb30b2b0a
kirill.yashmetov <k_yashmetov@mail.ru>
2026-04-10 08:39:47 +0300

    fix(admin): поправил редирект в админ портале

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,20 @@
-import type {NextConfig} from 'next'
+import {type NextConfig} from 'next'
 
-const nextConfig: NextConfig = {}
+const nextConfig: NextConfig = {
+	async redirects() {
+		return [
+			{
+				source: '/admin',
+				destination: '/admin/categories',
+				permanent: false,
+			},
+			{
+				source: '/admin/',
+				destination: '/admin/categories',
+				permanent: false,
+			},
+		]
+	},
+}
 
 export default nextConfig
