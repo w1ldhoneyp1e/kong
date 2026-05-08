cb5b02e2d157cb2e6df478b5bcfa5490907effc9
kirill.yashmetov <k_yashmetov@mail.ru>
2026-05-08 16:23:07 +0300

    refactor: удалил medusa

--- backend/src/api/_shared/getAuthHeader.ts
+++
@@ -1,8 +0,0 @@
-import {type MedusaRequest} from '@medusajs/framework'
-
-function getAuthHeader(req: MedusaRequest): string | undefined {
-	const headers = req.headers as unknown as Record<string, string>
-	return headers.authorization ?? headers.Authorization
-}
-
-export {getAuthHeader}
